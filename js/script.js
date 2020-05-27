be8ca1e309df01d04c7ac4a3008c76f834847063
andy <andy.ukpe@outlook.com>
2020-05-27 16:59:40 +0200

    Versione riassunta, dell'esercizio.

--- js/script.js
+++ js/script.js
@@ -5,29 +5,52 @@
 
 // SCRIVERE UN PROGRAMMA CHE STAMPI numeri
 // creo variabili
-var numero = 0;
-var lista = document.getElementById('lista-item');
-var testo;
+// var numero = 0;
+// var lista = document.getElementById('lista-item');
+// var testo;
+//
+// for (var i = 0; i < 100; i++) {
+//   // creare numeri da 1 a 100
+//   numero = numero + 1;
+//   // stampare numeri
+//   var itemLista = lista.innerHTML;
+//   lista.innerHTML = itemLista + '<li>' + numero + '</li>';
+//   // scrivere vicino ai multipli di 3 Fizz
+//   if( (numero % 3 == 0) && !( numero % 5 == 0 ) ){
+//     // scrivere Fizz
+//     testo = ' Fizz';
+//     lista.innerHTML = itemLista + '<li>' + testo + '</li>';
+//   } else if( (numero % 5 == 0 ) && !( numero % 3 == 0 )){// scrivere vicino ai multipli di 5 Buzz
+//     // scrivi Buzz
+//     testo = ' Buzz';
+//     lista.innerHTML = itemLista + '<li>' + testo + '</li>';
+//   } else if (( numero % 3 == 0) && ( numero % 5 == 0 )) {
+//     // scrivi Fizz Buzz
+//     testo = ' FizzBuzz';
+//     lista.innerHTML = itemLista + '<li>' + testo + '</li>';
+//   }
+// }
 
-for (var i = 0; i < 100; i++) {
+var lista = document.getElementById('lista-item');
+
+
+for (var i = 1; i <= 100; i++) {
   // creare numeri da 1 a 100
-  numero = numero + 1;
+  var numero = i;
+    var itemLista = lista.innerHTML;
   // stampare numeri
-  console.log(numero);
-  var itemLista = lista.innerHTML;
-  lista.innerHTML = itemLista + '<li>' + numero + '</li>';
   // scrivere vicino ai multipli di 3 Fizz
-  if( (numero % 3 == 0) && !( numero % 5 == 0 ) ){
+  if ( numero % 15 == 0) {// scrivere vicino ai multipli di 15 FizzBuzz
+    numero = 'FizzBuzz';
+  } else if ( numero % 3 == 0 ) {
     // scrivere Fizz
-    testo = ' Fizz';
-    lista.innerHTML = itemLista + '<li>' + numero + testo + '</li>';
-  } else if( (numero % 5 == 0 ) && !( numero % 3 == 0 )){// scrivere vicino ai multipli di 5 Buzz
+    numero = 'Fizz';
+  } else if( numero % 5 == 0 ) {// scrivere vicino ai multipli di 5 Buzz
     // scrivi Buzz
-    testo = ' Buzz';
-    lista.innerHTML = itemLista + '<li>' + numero + testo + '</li>';
-  } else if (( numero % 3 == 0) && ( numero % 5 == 0 )) {
-    // scrivi Fizz Buzz
-    testo = ' FizzBuzz';
-    lista.innerHTML = itemLista + '<li>' + numero + testo + '</li>';
+    numero = 'Buzz';
+  } else {
+    numero = numero
   }
+
+  lista.innerHTML = itemLista + '<li>' + numero + '</li>';
 }
