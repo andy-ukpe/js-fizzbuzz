55ff92f00a9a97bbea9bdff16e9bf0fa5dad5f7a
andy <andy.ukpe@outlook.com>
2020-05-27 15:08:14 +0200

    stampati numeri da 1 a 100

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli
+// di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// SCRIVERE UN PROGRAMMA CHE STAMPI numeri
+// creo variabili
+var numero = 0;
+var lista = document.getElementById('lista-item');
+
+for (var i = 0; i < 100; i++) {
+  // creare numeri da 1 a 100
+  numero = numero + 1;
+  // stampare numeri
+  console.log(numero);
+  var itemLista = lista.innerHTML;
+  lista.innerHTML = itemLista + '<li>' + numero + '</li>';
+}
